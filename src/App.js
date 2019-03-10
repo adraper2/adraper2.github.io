799600fadbd8e82760b5e9c5ec119c4964bf7029
Aidan Draper <adraper2@elon.edu>
2019-03-10 19:07:41 -0400

    added footer and more homepage info

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import NavBar from "./components/navbar";
+import Footer from "./components/footer";
 import "./App.css";
 
 class App extends Component {
@@ -9,7 +10,7 @@ class App extends Component {
 
   componentDidMount() {
     document.addEventListener("scroll", () => {
-      const isTop = window.scrollY < 100;
+      const isTop = window.scrollY < 50;
       if (isTop !== this.state.isTop) {
         this.onScroll(isTop);
       }
@@ -30,10 +31,24 @@ class App extends Component {
             <p>
               Hi, my name is Aidan! I am a senior studying Computer Science and
               Statistics at Elon University in North Carolina. I plan on
-              graduating this May and pursuing a Ph.D. afterwards.{" "}
+              graduating this May and pursuing a Ph.D. afterwards. My research
+              interests are in Machine Learning, Bayesian Statistics, and
+              general Classification Algorithms. I am particularly interested in
+              their applications in Computer Vision and Ecology.{" "}
+            </p>
+            <p>
+              <b>
+                Checkout{" "}
+                <a href="https://drive.google.com/file/d/1IGt0naMDSIeAdVNg4FpDtcW5Uj9f1M4h/view?usp=sharing">
+                  the preprint
+                </a>{" "}
+                of the article that I just submitted to the NC Journal of
+                Mathematics and Statistics!{" "}
+              </b>
             </p>
           </div>
         </main>
+        <Footer />
       </React.Fragment>
     );
   }
---
+++ src/components/footer.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import reactLogo from "../logo.svg";
+import fb_image from "../content/Facebook.png";
+import github_image from "../content/Github.png";
+import insta_image from "../content/Instagram.png";
+import link_image from "../content/Linkedin.png";
+import twit_image from "../content/Twitter.png";
+
+class Footer extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="footer">
+        <hr />
+        <div align="center" className="footer-element">
+          <a
+            className="account_links"
+            href="https://www.facebook.com/aidan.draper"
+          >
+            <img src={fb_image} className="accounts" />
+          </a>
+          <a className="account_links" href="https://github.com/adraper2">
+            <img src={github_image} className="accounts" />
+          </a>
+          <a
+            className="account_links"
+            href="https://www.instagram.com/adraper19/"
+          >
+            <img src={insta_image} className="accounts" />
+          </a>
+          <a
+            className="account_links"
+            href="https://www.linkedin.com/in/aidan-draper/"
+          >
+            <img src={link_image} className="accounts" />
+          </a>
+          <a className="account_links" href="https://twitter.com/adraper19">
+            <img src={twit_image} className="accounts" />
+          </a>
+        </div>
+        <div align="center" className="footer-element">
+          Aidan James Draper &copy; 2019
+        </div>
+        <div align="center" className="footer-element">
+          <img src={reactLogo} className="App-logo" id="reactLogo" />
+          Built using React.{" "}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Footer;
