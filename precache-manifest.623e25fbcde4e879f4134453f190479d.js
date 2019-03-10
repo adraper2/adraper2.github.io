c533362daff2f4bd08034d86fd2ece5c03ce8c9f
Aidan Draper <adraper2@elon.edu>
2019-03-10 17:14:57 -0400

    Updates

---
+++ precache-manifest.623e25fbcde4e879f4134453f190479d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "eee9456677e1b61672ac",
+    "url": "/static/js/main.2e26e6d0.chunk.js"
+  },
+  {
+    "revision": "745fcb33c971c0658402",
+    "url": "/static/js/2.5a97aec8.chunk.js"
+  },
+  {
+    "revision": "eee9456677e1b61672ac",
+    "url": "/static/css/main.0df90713.chunk.css"
+  },
+  {
+    "revision": "745fcb33c971c0658402",
+    "url": "/static/css/2.90860d4a.chunk.css"
+  },
+  {
+    "revision": "db3b36b7f589db80c98fb527d42c40e4",
+    "url": "/index.html"
+  }
+];
